8095d425b123989394d7c745c99369aceba6addc
Ghost5454 <adam1petchey@gmail.com>
2022-10-10 14:52:02 +0100

    Basic binary tree

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node root = new Node(0);
+
+        root.AddToLeft(new Node(1));
+        root.AddToRight(new Node(2));
+        root.left.AddToLeft(new Node(3));
+        root.left.AddToRight(new Node(4));
+        root.right.AddToLeft(new Node(5));
+        root.right.AddToRight(new Node(6));
+        root.left.left.AddToLeft(new Node(7));
+
+        System.out.println(root.data);
+        System.out.println(root.left.data + " | " + root.right.data);
+        System.out.println(root.left.left.data + " | " + root.left.right.data + " || " + root.right.left.data + " | " +root.right.right.data);
+        System.out.println(root.left.left.left.data);
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,17 +1,47 @@
 public class Node {
 
     public int data;
+    public boolean isLeaf;
+    public Node left, right;
 
-    private  Node _left, _right;
+    Node(int data)
+    {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+        this.isLeaf = true;
+    }
 
     public void AddToLeft(Node myNode)
     {
-        _left = myNode;
+        left = myNode;
+        isLeaf = false;
     }
 
     public void AddToRight(Node myNode)
     {
-        _right = myNode;
+        right = myNode;
+        isLeaf = false;
+    }
+
+    public Node GetLeft()
+    {
+        if (isLeaf)
+        {
+            System.out.println("Is a leaf");
+        }
+
+        return left;
+    }
+
+    public Node GetRight()
+    {
+        if (isLeaf)
+        {
+            System.out.println("Is a leaf");
+        }
+
+        return right;
     }
 
 }
